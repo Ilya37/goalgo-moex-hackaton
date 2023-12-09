a20884d9c2bb60f823ad3051899a9c721b0f4e11
Insaf Ashrapov <iashrapov@gmail.com>
2023-12-09 17:02:11 +0300

    added docstrings

--- strategies.py
+++ strategies.py
@@ -6,16 +6,35 @@ from backtesting.test import SMA, GOOG
 
 
 def std_3(arr, n):
+    """
+    Calculate 3 standard deviations for a rolling window on the data.
+
+    Parameters:
+    arr (array-like): Input data
+    n (int): Size of the rolling window
+
+    Returns:
+    pd.Series: Resulting data with rolling standard deviation
+    """
     return pd.Series(arr).rolling(n).std() * 3
 
 
 class SmaCross(Strategy):
+    """
+    Implements the SMA Cross strategy.
+    """
     def init(self):
+        """
+        Initialize the strategy. Calculate the SMA for two different periods.
+        """
         price = self.data.Close
         self.ma1 = self.I(SMA, price, 10)
         self.ma2 = self.I(SMA, price, 20)
 
     def next(self):
+        """
+        Define the strategy's step. Buy when the first SMA crosses above the second, sell when it crosses below.
+        """
         if crossover(self.ma1, self.ma2):
             self.buy()
         elif crossover(self.ma2, self.ma1):
@@ -23,9 +42,15 @@ class SmaCross(Strategy):
 
 
 class MeanReversion(Strategy):
+    """
+    Implements the Mean Reversion strategy.
+    """
     roll = 50
 
     def init(self):
+        """
+        Initialize the strategy. Calculate the mean and standard deviation for a rolling window on the data.
+        """
         self.he = self.data.Close
 
         self.he_mean = self.I(SMA, self.he, self.roll)
@@ -36,6 +61,9 @@ class MeanReversion(Strategy):
         self.he_close = self.I(SMA, self.he, 1)
         
     def next(self):
+        """
+        Define the strategy's step. Buy when the price is below the lower bound, sell when it's above the upper bound.
+        """
 
         if self.he_close < self.he_lower:
             self.buy(
@@ -49,6 +77,15 @@ class MeanReversion(Strategy):
 
 
 def run_sma_cross_strategy_stats(df):
+    """
+    Run the SMA Cross strategy on the data and return the statistics.
+
+    Parameters:
+    df (pd.DataFrame): Input data
+
+    Returns:
+    dict: Statistics of the strategy
+    """
     sma_cross_strategy = Backtest(df, SmaCross, commission=.002,
                                   exclusive_orders=True)
     stats = sma_cross_strategy.run()
@@ -56,6 +93,15 @@ def run_sma_cross_strategy_stats(df):
 
 
 def run_sma_cross_strategy_plot(df):
+    """
+    Run the SMA Cross strategy on the data and plot the results.
+
+    Parameters:
+    df (pd.DataFrame): Input data
+
+    Returns:
+    matplotlib.figure.Figure: Plot of the strategy
+    """
     sma_cross_strategy = Backtest(df, SmaCross, commission=.002,
                                   exclusive_orders=True)
     sma_cross_strategy.run()
@@ -68,6 +114,15 @@ def run_sma_cross_strategy_plot(df):
 
 
 def run_mean_reversion_stats(df):
+    """
+    Run the Mean Reversion strategy on the data and return the statistics.
+
+    Parameters:
+    df (pd.DataFrame): Input data
+
+    Returns:
+    dict: Statistics of the strategy
+    """
     mean_reversion_strategy = Backtest(df, MeanReversion, commission=.002,
                                        exclusive_orders=True)
     stats = mean_reversion_strategy.run()
@@ -75,6 +130,15 @@ def run_mean_reversion_stats(df):
 
 
 def run_mean_reversion_plot(df):
+    """
+    Run the Mean Reversion strategy on the data and plot the results.
+
+    Parameters:
+    df (pd.DataFrame): Input data
+
+    Returns:
+    matplotlib.figure.Figure: Plot of the strategy
+    """
     mean_reversion_strategy = Backtest(df, MeanReversion, commission=.002,
                                        exclusive_orders=True)
     mean_reversion_strategy.run()
